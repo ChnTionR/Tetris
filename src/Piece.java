b86c6f9dc9b58663c62d14d9e9b19bca810b17b6
Çınar Kaya <cnrkaya008@gmail.com>
2024-09-07 23:14:41 +0300

    different textured pieces

--- src/Piece.java
+++ src/Piece.java
@@ -150,23 +150,35 @@ public class Piece{
 
     public void drawPixel(Graphics g, Map.Entry<Rectangle, Color> entry){
         Graphics2D g2D = (Graphics2D) g;
-        int[] topSideX = {entry.getKey().x, entry.getKey().x+10, entry.getKey().x+20};
-        int[] topSideY = {entry.getKey().y, entry.getKey().y+10, entry.getKey().y};
+        int[] topSideX = {entry.getKey().x, entry.getKey().x+4, entry.getKey().x+16, entry.getKey().x+20};
+        int[] topSideY = {entry.getKey().y, entry.getKey().y+4, entry.getKey().y+4, entry.getKey().y};
 
-        int[] sideSideX = {entry.getKey().x, entry.getKey().x+20, entry.getKey().x+20, entry.getKey().x};
-        int[] sideSideY = {entry.getKey().y,entry.getKey().y+20, entry.getKey().y, entry.getKey().y+20};
+        int[] leftSideX = {entry.getKey().x, entry.getKey().x+4, entry.getKey().x+4, entry.getKey().x};
+        int[] leftSideY = {entry.getKey().y,entry.getKey().y+4, entry.getKey().y+16, entry.getKey().y+20};
 
-        int[] bottomSideX = {entry.getKey().x, entry.getKey().x+10, entry.getKey().x+20};
-        int[] bottomSideY = {entry.getKey().y+20, entry.getKey().y+10, entry.getKey().y+20};
+        int[] rightSideX = {entry.getKey().x+20, entry.getKey().x+16, entry.getKey().x+16, entry.getKey().x+20};
+        int[] rightSideY = {entry.getKey().y,entry.getKey().y+4, entry.getKey().y+16, entry.getKey().y+20};
+
+        int[] bottomSideX = {entry.getKey().x, entry.getKey().x+4, entry.getKey().x+16, entry.getKey().x+20};
+        int[] bottomSideY = {entry.getKey().y+20, entry.getKey().y+16, entry.getKey().y+16, entry.getKey().y+20};
+
+        int[] middleX = {entry.getKey().x+4, entry.getKey().x+16, entry.getKey().x+16, entry.getKey().x+4};
+        int[] middleY = {entry.getKey().y+4,entry.getKey().y+4, entry.getKey().y+16, entry.getKey().y+16};
 
         g2D.setColor(new Color(180,180,180));
-        g2D.fillPolygon(topSideX, topSideY, 3);
+        g2D.fillPolygon(topSideX, topSideY, 4);
 
-        g2D.setColor(new Color(150,150,150));
-        g2D.fillPolygon(sideSideX, sideSideY, 4);
+        g2D.setColor(new Color(160,160,160));
+        g2D.fillPolygon(leftSideX, leftSideY, 4);
+
+        g2D.setColor(new Color(140,140,140));
+        g2D.fillPolygon(middleX, middleY, 4);
 
         g2D.setColor(new Color(80,80,80));
-        g2D.fillPolygon(bottomSideX, bottomSideY, 3);
+        g2D.fillPolygon(rightSideX, rightSideY, 4);
+
+        g2D.setColor(new Color(50,50,50));
+        g2D.fillPolygon(bottomSideX, bottomSideY, 4);
 
         g2D.setColor(entry.getValue());
         g2D.fill(new Rectangle(entry.getKey().x, entry.getKey().y, 20,20));
